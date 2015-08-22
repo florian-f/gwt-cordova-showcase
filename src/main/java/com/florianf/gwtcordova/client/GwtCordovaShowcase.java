bfc8149445ef14744b03d3a6799689e4b3ad5ae5
Florian Friemel <florianfriemel@gmail.com>
2015-08-22 21:41:00 +0200

    project skeleton

---
+++ src/main/java/com/florianf/gwtcordova/client/GwtCordovaShowcase.java
@@ -0,0 +1,10 @@
+package com.florianf.gwtcordova.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class GwtCordovaShowcase implements EntryPoint {
+
+  public void onModuleLoad() {
+
+  }
+}
