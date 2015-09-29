610ca0fa3f6ea3cd02d8346d5a744c3c03bcab8c
Florian Friemel <florianfriemel@gmail.com>
2015-09-29 21:35:00 +0200

    simple build script

---
+++ build.sh
@@ -0,0 +1,4 @@
+mvn clean gwt:compile war:exploded;
+rm -r app/www/*;
+rm target/gwtcordova-showcase-0.1-SNAPSHOT/gwtcordovashowcase/bower_components/web-animations-js/web-animations.min.js.gz;
+cp -r target/gwtcordova-showcase-0.1-SNAPSHOT/* app/www/;
