4646e9e9dfbb1d5143619b282d2185d6839ff395
Florian Friemel <florianfriemel@gmail.com>
2015-09-29 21:36:00 +0200

    works with gwt-cordova 0.1!

--- src/main/java/com/florianf/gwtcordovashowcase/client/GwtCordovaShowcase.java
+++ src/main/java/com/florianf/gwtcordovashowcase/client/GwtCordovaShowcase.java
@@ -1,8 +1,7 @@
 package com.florianf.gwtcordovashowcase.client;
 
-import com.florianf.gwtcordova.client.PhoneGap;
-import com.florianf.gwtcordova.client.PhoneGapAvailableEvent;
-import com.florianf.gwtcordova.client.PhoneGapStandardImpl;
+import com.florianf.gwtcordova.client.Cordova;
+import com.florianf.gwtcordova.client.DeviceReadyEvent;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -16,30 +15,30 @@ public class GwtCordovaShowcase implements EntryPoint {
 
     public void onModuleLoad() {
 
-        PhoneGapStandardImpl phoneGap = GWT.create(PhoneGapStandardImpl.class);
-
-        phoneGap.addHandler((PhoneGapAvailableEvent event) -> {
-            Polymer.importHref(Arrays.asList(
-                    "iron-icons/iron-icons.html",
-                    PaperIconItemElement.SRC,
-                    PaperRippleElement.SRC,
-                    IronIconElement.SRC,
-                    PaperDrawerPanelElement.SRC,
-                    PaperHeaderPanelElement.SRC,
-                    PaperToolbarElement.SRC,
-                    PaperFabElement.SRC,
-                    PaperDialogElement.SRC,
-                    PaperTextareaElement.SRC,
-                    PaperInputElement.SRC,
-                    PaperButtonElement.SRC,
-                    PaperCheckboxElement.SRC
-            ), arg -> {
-                startApplication();
-                return null;
-            });
+        Cordova.init(new DeviceReadyEvent.Listener() {
+            @Override
+            protected void handleEvent(DeviceReadyEvent deviceReadyEvent) {
+                Polymer.importHref(Arrays.asList(
+                        "iron-icons/iron-icons.html",
+                        PaperIconItemElement.SRC,
+                        PaperRippleElement.SRC,
+                        IronIconElement.SRC,
+                        PaperDrawerPanelElement.SRC,
+                        PaperHeaderPanelElement.SRC,
+                        PaperToolbarElement.SRC,
+                        PaperFabElement.SRC,
+                        PaperDialogElement.SRC,
+                        PaperTextareaElement.SRC,
+                        PaperInputElement.SRC,
+                        PaperButtonElement.SRC,
+                        PaperCheckboxElement.SRC
+                ), arg -> {
+                    startApplication();
+                    return null;
+                });
+            }
         });
 
-        phoneGap.initializePhoneGap();
 
     }
 
