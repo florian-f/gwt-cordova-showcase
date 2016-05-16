b82b61f3a418f912adcb52390820e5c9869e222f
Florian Friemel <florianfriemel@gmail.com>
2016-05-16 21:20:00 +0200

    fix syntax error

--- src/main/java/com/florianf/gwtcordovashowcase/client/CameraSample.java
+++ src/main/java/com/florianf/gwtcordovashowcase/client/CameraSample.java
@@ -5,7 +5,7 @@ import com.florianf.gwtcordova.client.plugin.camera.CameraConstants;
 import com.florianf.gwtcordova.client.plugin.camera.CameraOptions;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.ImageElement;
-import com.google.gwt.dom.client.SpanElement;;
+import com.google.gwt.dom.client.SpanElement;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Button;
