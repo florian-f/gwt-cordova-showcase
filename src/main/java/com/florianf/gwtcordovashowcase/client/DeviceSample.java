57c8007e7dcfb5e3e0726685fd1be4691633784a
Florian Friemel <florianfriemel@gmail.com>
2016-05-18 21:17:00 +0200

    DeviceSample: use new onReady method

--- src/main/java/com/florianf/gwtcordovashowcase/client/DeviceSample.java
+++ src/main/java/com/florianf/gwtcordovashowcase/client/DeviceSample.java
@@ -1,5 +1,8 @@
 package com.florianf.gwtcordovashowcase.client;
 
+import com.florianf.gwtcordova.client.Cordova;
+import com.florianf.gwtcordova.client.myelemental.Event;
+import com.florianf.gwtcordova.client.myelemental.EventListener;
 import com.florianf.gwtcordova.client.plugin.device.Device;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -24,45 +27,117 @@ public class DeviceSample extends Composite {
 
     public DeviceSample() {
         initWidget(deviceSampleUiBinder.createAndBindUi(DeviceSample.this));
-        retry();
-        retryButton.addClickHandler(clickEvent -> retry());
-        devicePanel.add(retryButton);
+
+        Cordova.onReady(event -> {
+            String available;
+            String platform;
+            String version;
+            String uuid;
+            String cordova;
+            String model;
+            String manufacturer;
+
+            try {
+                platform = Device.getPlatform();
+                available = Device.getAvailable();
+                version = Device.getVersion();
+                uuid = Device.getUuid();
+                cordova = Device.getCordova();
+                model = Device.getModel();
+                manufacturer = Device.getManufacturer();
+            } catch (Exception e) {
+                available = "error";
+                platform = "error";
+                version = "error";
+                uuid = "error";
+                cordova = "error";
+                model = "error";
+                manufacturer = "error";
+            }
+            devicePanel.add(new Label("Platform: " + platform));
+            devicePanel.add(new Label("Cordova: " + cordova));
+            devicePanel.add(new Label("Manufacturer: " + manufacturer));
+            devicePanel.add(new Label("Model: " + model));
+            devicePanel.add(new Label("Uuid: " + uuid));
+            devicePanel.add(new Label("Available: " + available));
+            devicePanel.add(new Label("Version: " + version));
+        });
+//        retry();
+//        retryButton.addClickHandler(clickEvent -> retry());
+//        devicePanel.add(retryButton);
 
     }
 
-    public void retry() {
+//    public void retry() {
+//
+//        String available;
+//        String platform;
+//        String version;
+//        String uuid;
+//        String cordova;
+//        String model;
+//        String manufacturer;
+//
+//        try {
+//            platform = Device.getPlatform();
+//            available = Device.getAvailable();
+//            version = Device.getVersion();
+//            uuid = Device.getUuid();
+//            cordova = Device.getCordova();
+//            model = Device.getModel();
+//            manufacturer = Device.getManufacturer();
+//        } catch (Exception e) {
+//            available = "error";
+//            platform = "error";
+//            version = "error";
+//            uuid = "error";
+//            cordova = "error";
+//            model = "error";
+//            manufacturer = "error";
+//        }
+//        devicePanel.add(new Label("Platform: " + platform));
+//        devicePanel.add(new Label("Cordova: " + cordova));
+//        devicePanel.add(new Label("Manufacturer: " + manufacturer));
+//        devicePanel.add(new Label("Model: " + model));
+//        devicePanel.add(new Label("Uuid: " + uuid));
+//        devicePanel.add(new Label("Available: " + available));
+//        devicePanel.add(new Label("Version: " + version));
+//    }
 
-        String available;
-        String platform;
-        String version;
-        String uuid;
-        String cordova;
-        String model;
-        String manufacturer;
 
-        try {
-            platform = Device.getPlatform();
-            available = Device.getAvailable();
-            version = Device.getVersion();
-            uuid = Device.getUuid();
-            cordova = Device.getCordova();
-            model = Device.getModel();
-            manufacturer = Device.getManufacturer();
-        } catch (Exception e) {
-            available = "error";
-            platform = "error";
-            version = "error";
-            uuid = "error";
-            cordova = "error";
-            model = "error";
-            manufacturer = "error";
-        }
-        devicePanel.add(new Label("Platform: " + platform));
-        devicePanel.add(new Label("Cordova: " + cordova));
-        devicePanel.add(new Label("Manufacturer: " + manufacturer));
-        devicePanel.add(new Label("Model: " + model));
-        devicePanel.add(new Label("Uuid: " + uuid));
-        devicePanel.add(new Label("Available: " + available));
-        devicePanel.add(new Label("Version: " + version));
-    }
+//    {
+//
+//        String available;
+//        String platform;
+//        String version;
+//        String uuid;
+//        String cordova;
+//        String model;
+//        String manufacturer;
+//
+//        try {
+//            platform = Device.getPlatform();
+//            available = Device.getAvailable();
+//            version = Device.getVersion();
+//            uuid = Device.getUuid();
+//            cordova = Device.getCordova();
+//            model = Device.getModel();
+//            manufacturer = Device.getManufacturer();
+//        } catch (Exception e) {
+//            available = "error";
+//            platform = "error";
+//            version = "error";
+//            uuid = "error";
+//            cordova = "error";
+//            model = "error";
+//            manufacturer = "error";
+//        }
+//        devicePanel.add(new Label("Platform: " + platform));
+//        devicePanel.add(new Label("Cordova: " + cordova));
+//        devicePanel.add(new Label("Manufacturer: " + manufacturer));
+//        devicePanel.add(new Label("Model: " + model));
+//        devicePanel.add(new Label("Uuid: " + uuid));
+//        devicePanel.add(new Label("Available: " + available));
+//        devicePanel.add(new Label("Version: " + version));
+//    }
 }
